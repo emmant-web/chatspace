f05806264e075abb3e438767cf35dd8ef0395b87
emmant-web <emmanueldtoribio@gmail.com>
2026-01-23 15:08:01 +0800

    finished barebones of landing page navbar

--- src/components/landing/LandingNavbar.tsx
+++ src/components/landing/LandingNavbar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router";
-import {X, Menu} from 'lucide-react'
-
+import { X, Menu } from "lucide-react";
 
 function LandingNavbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -10,23 +9,26 @@ function LandingNavbar() {
     // how to build navbar - logo, mobile right side (use md:hidden), then for tablet and desktop show the two buttons (sign up and login)
     <nav className="bg-red-100 px-3 py-6 ">
       <div className="flex justify-between">
-        <div>
+        <div className="flex justify-between w-full">
+          <div className="flex justify-between gap-8">
+            <Link to="/">chatspace</Link>
 
-            <div><Link to="/">chatspace</Link></div>
-            <div></div>
-
-
-        </div>
-
-        {/* desktop buttons */}
-        <div className="sm: hidden">
-          <Link to="/"></Link>
-          <Link to="/"></Link>
+            <div className="sm: hidden lg:flex lg:gap-4">
+              <a href="#features">Features</a>
+              <a href="#how-it-works">How It Works</a>
+              <a href="#faq">FAQ</a>
+            </div>
+          </div>
+          {/* desktop buttons */}
+          <div className="sm: hidden md: hidden lg:flex lg:gap-4">
+            <Link to="">Sign Up</Link>
+            <Link to="">Login</Link>
+          </div>
         </div>
 
         {/* mobile burger icon */}
         <button
-          className="bg-green md:hidden"
+          className="bg-green lg:hidden"
           onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
         >
           {mobileMenuOpen ? <X /> : <Menu />}
@@ -35,9 +37,12 @@ function LandingNavbar() {
 
       {/* mobile menu - opening mobile menu */}
       {mobileMenuOpen && (
-        <div>
-          <Link to=""></Link>
-          <Link to=""></Link>
+        <div className="flex flex-col">
+          <a href="#features">Features</a>
+          <a href="#how-it-works">How It Works</a>
+          <a href="#faq">FAQ</a>
+          <Link to="">Sign Up</Link>
+          <Link to="">Login</Link>
         </div>
       )}
     </nav>
