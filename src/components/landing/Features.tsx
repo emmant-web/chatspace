32ab7246899e2dc718dc390c2542e335622d07d3
emmant-web <emmanueldtoribio@gmail.com>
2026-01-24 12:13:56 +0800

    added boilerplate for features section

---
+++ src/components/landing/Features.tsx
@@ -0,0 +1,17 @@
+function Features() {
+  return (
+    <section>
+      {/* features header */}
+      <div></div>
+
+      {/* grid for features */}
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  );
+}
+
+export default Features;
